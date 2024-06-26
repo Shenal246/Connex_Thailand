f1dd008020c8436fb6b18c60ca960b06b8fa54fb
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-06-26 15:29:47 +0530

    comit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,6 +8,9 @@ import Slider from "react-slick";
 import 'animate.css';
 import card1 from '../../images/map/map trans.png';
 import { useTranslation } from 'react-i18next';
+import ThaiNavbar from '../Navbar/Navbar';
+import ThaiChatbot from '../ChatBot/Chat';
+import ThaiFooter from '../Footer/Footer';
 
 function SampleNextArrow(props) {
     const { className, style, onClick } = props;
@@ -113,6 +116,8 @@ const About = () => {
 
     return (
         <>
+            <ThaiNavbar />
+            <ThaiChatbot />
             <section id='t1'>
                 <div class="container classss">
                     <div className='center' data-aos="fade-up" data-aos-delay="200">
@@ -209,7 +214,7 @@ const About = () => {
                     <div className="col-2" ></div>
                 </div>
             </div>
-
+            <ThaiFooter />
         </>);
 
 
